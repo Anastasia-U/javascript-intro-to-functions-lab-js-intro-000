88d4be2e1312c2ed478a19b9fe1a6e7fb302fc29
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-07-12 17:28:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,10 +21,13 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
+
   if (string === string.toUpperCase()) {
     return `YES INDEED!`
   }
+
   else if (string === "I love you, Grandma")
     return `I love you, too.`
   }
+  
 }
